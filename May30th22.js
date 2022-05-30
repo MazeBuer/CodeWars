28246ed105ddf839134a9bfa42298a671ab1e9fd
MazeBuer <mazikeenbuer@gmail.com>
2022-05-30 00:17:22 -0700

    daily add

---
+++ May30th22.js
@@ -0,0 +1,8 @@
+/*You will be given an array a and a value x. Check whether the provided array contains the value. return true or false
+P: we will be given an array and a value. The value x and array a will be a number or a string. 
+R: return true or flase if x is in array a
+E: [66,101,20] x=66 -> true
+P: use .includes () method to check if x is in a. */
+
+let check = (a,x) => a.includes(x)
+
