9b29c4121b36ed7d2acb3c11be3df9f015ba5704
MazeBuer <mazikeenbuer@gmail.com>
2022-06-19 00:16:27 -0700

    daily commit

---
+++ June19th22.js
@@ -0,0 +1,7 @@
+/* write a function that takes an argument and returns the square of it
+P: will the argument always be an integer? will it be a string in an integer?
+R: return the square of the argument given
+E: 3 -> 9
+P: take num and square it. Can use Math.pow */
+
+let square = num => Math.pow(num,2)
