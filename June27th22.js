78c44164f4f4ae6bb6518f8a1e4edb382858e153
MazeBuer <mazikeenbuer@gmail.com>
2022-06-29 17:14:26 -0700

    changes

--- June27th22.js
+++ June27th22.js
@@ -4,3 +4,20 @@ function accum(s) {
 }
 
 console.log(accum('abc'))
+
+
+function digital_root(n) {
+    let num = Array.from(String(n), Number)
+    let a = num.reduce((a,b) => a+b,0)
+    let answer = Array.from(String(a),Number)
+    for(let i=0; i <= answer; i++) {
+      if(answer.length > 1) {
+        return num.reduce((a,b) => a+b,0)
+      }else {
+        return answer
+      }
+    }
+    
+  }
+
+  console.log(digital_root(567))
