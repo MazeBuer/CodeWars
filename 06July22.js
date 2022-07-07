4faa5c7581712c9a88f75d58128ad68e0dcb07b2
MazeBuer <mazikeenbuer@gmail.com>
2022-07-06 21:40:26 -0700

    daily challenge

---
+++ 06July22.js
@@ -0,0 +1,11 @@
+/*The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+P: we will be given a name; could be single or full;
+R: return 'Hello name'
+E: Hello, Mr. Spock
+P: concatnate with + */
+
+function sayHello(name) {
+    return 'Hello, ' + name
+}
+
+console.log(sayHello('susy'))
