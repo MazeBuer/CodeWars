c991041e799888d69bb90d77d1a6b78ba7e40df6
MazeBuer <mazikeenbuer@gmail.com>
2022-07-31 22:56:31 -0700

    daily challenge

---
+++ July31st22.js
@@ -0,0 +1,12 @@
+/* Implement a function which convert the given boolean value into its string representation. 
+P: you are given a boolean and convert into a string
+R: return a string
+E: true -> 'true'
+P: .toString or add '' to each
+*/
+
+function booleanToString(b){
+  return b.toString()
+}
+
+console.log(booleanToString(true))
