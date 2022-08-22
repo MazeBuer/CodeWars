c40abd3134edbef820bf3f9132463cb925f2a847
MazeBuer <mazikeenbuer@gmail.com>
2022-08-22 00:19:09 -0700

    daily challenge

---
+++ 114.js
@@ -0,0 +1,9 @@
+/* Write a function which calculates the average of the numbers in a given list.
+P: will be given a list of numbers in array; will it ever be empty? yes return 0; 
+R: return the average of numbers in the array
+E: [3,4,5] -> 4 
+P: add all integers in the array with reduce(); divide by length of array; */
+
+let find_average = array => array.reduce( (a,b) => a+b, 0) / array.length;
+
+console.log(find_average([1,3,5,6]))
